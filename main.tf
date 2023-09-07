984b81c9945142060f442467e5886497f4a997d1
johnny-menezes <47942673+johnny-menezes@users.noreply.github.com>
2023-09-07 17:55:57 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,21 @@
 module "aws_rds" {
   source = "github.com/pocacc/aws-modules-demo/aws_db_instance_module/"
   
-  allocated_storage    = var.allocated_storage 
-  db_name              = var.db_name
-  engine               = var.engine
-  engine_version       = var.engine_version
-  instance_class       = var.instance_class
-  username             = var.username
-  password             = var.password 
-  parameter_group_name = var.parameter_group_name
-  skip_final_snapshot  = var.skip_final_snapshot
+  identifier             = var.identifier
+  allocated_storage      = var.allocated_storage
+  storage_type           = var.storage_type
+  engine                 = var.engine
+  engine_version         = var.engine_version
+  instance_class         = var.instance_class
+  username               = var.username
+  password               = var.password
+  port                   = var.port
+  skip_final_snapshot    = var.skip_final_snapshot
+ 
+  tags = {
+      Name        = var.name
+      Environment = var.environment
+      Provisioner = "Terraform"
+     
+    }
 }
