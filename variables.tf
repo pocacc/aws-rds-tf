12d7bf680f5ad12d9600a025d8846131037d19b4
johnny-menezes <47942673+johnny-menezes@users.noreply.github.com>
2023-09-07 17:54:20 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,47 +1,72 @@
-variable "allocated_storage" {
-type    = number
-default = 20
-
+//Resource/////////////////
+variable "identifier" {
+  description = "The name of the RDS instance"
+  type        = string
+  default     = "tf-database-01"
 }
 
-variable "db_name" {
-type   = string
-  
+variable "name" {
+  description = "Name of the TAG Database"
+  type        =  string
+  default     = "tf-database"
+}
+
+variable "allocated_storage" {
+  description = "The allocated storage in gigabytes"
+  type        = string
+  default     = 10 
+}
+
+variable "storage_type" {
+  description = "Type of Storage to be used"
+  type        = string
+  default     = "gp2"
 }
 
 variable "engine" {
-type = string
-default = "mysql"
-  
+  description = "The database engine to use"
+  type        = string
+  default     = "mysql"
 }
 
 variable "engine_version" {
-type = string
-  
+  description = "The engine version to use"
+  type        = string
+  default     = "5.7"
 }
 
 variable "instance_class" {
-type = string
-  
+  description = "The instance type of the RDS instance"
+  type        = string
+  default     = "db.t3.micro" 
 }
 
 variable "username" {
-type = string
-  
+  description = "Username for the master DB user"
+  type        = string
+  default     = "admin"
 }
 
 variable "password" {
-type = string
-
+  description = "valuePassword for the master DB user"
+  type        = string
+  default     = "jq%wdS5C!AB4" 
 }
 
-variable "parameter_group_name" {
-type = string
-  
+variable "port" {
+  description = "The port on which the DB accepts connections"
+  type        = string
+  default     = "3306"
 }
 
 variable "skip_final_snapshot" {
-type = bool
-default = false
-  
+  description = "Determines whether a final DB snapshot is created before the DB instance is deleted. If true is specified, no DBSnapshot is created. If false is specified, a DB snapshot is created before the DB instance is deleted"
+  type = bool
+  default = true
+}
+
+variable "environment" {
+     description = "Environment of the application"
+     type        =  string
+     default     = "test"  
 }
